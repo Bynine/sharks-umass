cd2615c3aecd71df87b42e49642601690afca4a5
Ben Kaufman <benkaufman@1x-nat-vl930-172-30-108-16.wireless.umass.edu>
2017-04-18 21:38:19 -0400

    added Definer and DefinerResult classes

---
+++ app/src/test/java/sharks_umass/scanit/DefinerUnitTest.java
@@ -0,0 +1,22 @@
+package sharks_umass.scanit;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+public class DefinerUnitTest {
+    @Test
+    public void testDefiner() {
+        Definer d = new Definer();
+        DefinerResult res = d.define("winning");
+        assertNotNull(res);
+        assertNotNull(res.getWord());
+        assertNotNull(res.getDefinition());
+        assertNotNull(res.getExample());
+        System.out.println(res.getWord());
+        System.out.println(res.getDefinition());
+        System.out.println(res.getExample());
+    }
+}
