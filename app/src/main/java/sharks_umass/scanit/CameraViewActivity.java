2b380dc1f0621f16036a4d19390ee5d66ae96d4e
Mayank Kumar <mayankk2308@gmail.com>
2017-04-19 14:25:32 -0400

    Initial setup + Update UI with camera button

--- app/src/main/java/sharks_umass/scanit/CameraViewActivity.java
+++ app/src/main/java/sharks_umass/scanit/CameraViewActivity.java
@@ -10,27 +10,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class CameraViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Finish Project", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
     }
 
     @Override
