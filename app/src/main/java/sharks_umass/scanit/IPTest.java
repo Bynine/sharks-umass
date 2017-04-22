2bee8d538f4232291a5649d05949897337110de1
Bynine <byninegiga@gmail.com>
2017-04-22 18:00:39 -0400

    Added test files, updated build

---
+++ app/src/main/java/sharks_umass/scanit/IPTest.java
@@ -0,0 +1,25 @@
+package sharks_umass.scanit;
+
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+
+import sharks_umass.scanit.apis.ImageProcessor;
+
+/**
+ * Created by Tyler on 4/18/2017.
+ */
+public class IPTest {
+//    @Test
+//    public void testByteBuffer() {
+//        // TODO: make context
+//        ImageProcessor ip = new ImageProcessor(null);
+//        File file = new File("drawable/testimage.png");
+//        int size = (int) file.length();
+//        byte[] bytes = new byte[size];
+//        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+//        ByteBuffer.allocateDirect(size);
+//        System.out.println(ip.convertInputToText(buffer, 59959595, 59959595, ImageProcessor.NV21));
+//    }
+}
