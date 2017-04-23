02685dff9f57d8f13f8a88e3d6ce6d1b83389255
Mayank Kumar <mayankk2308@gmail.com>
2017-04-23 15:35:45 -0400

    Setup camera view

--- app/src/main/java/sharks_umass/scanit/CameraViewActivity.java
+++ app/src/main/java/sharks_umass/scanit/CameraViewActivity.java
@@ -1,6 +1,11 @@
 package sharks_umass.scanit;
 
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,10 +17,32 @@ import android.view.MenuItem;
 
 public class CameraViewActivity extends AppCompatActivity {
 
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e) {
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        ImageButton clickPhoto = (ImageButton) findViewById(R.id.click_photo);
+        clickPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("ACTION", "Clicked a photo");
+            }
+        });
     }
 
     @Override
