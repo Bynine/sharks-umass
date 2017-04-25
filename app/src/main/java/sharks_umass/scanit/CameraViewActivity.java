1511a941748140186cccf7786e6c564a1ebb16b3
Mayank Kumar <mayankk2308@gmail.com>
2017-04-25 16:30:37 -0400

    Organizational commit

--- app/src/main/java/sharks_umass/scanit/CameraViewActivity.java
+++ app/src/main/java/sharks_umass/scanit/CameraViewActivity.java
@@ -3,20 +3,12 @@ package sharks_umass.scanit;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,8 +33,6 @@ import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.widget.Toast;
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -53,7 +43,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.ThreadFactory;
 
 public class CameraViewActivity extends AppCompatActivity {
 
