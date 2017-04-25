4c433045dc17ab3f120388ef61145db509679562
Bynine <byninegiga@gmail.com>
2017-04-25 17:03:48 -0400

    Updated ImageProcessor and added corresponding compile to build.gradle

--- app/src/main/java/sharks_umass/scanit/apis/ImageProcessor.java
+++ app/src/main/java/sharks_umass/scanit/apis/ImageProcessor.java
@@ -1,18 +1,58 @@
 package sharks_umass.scanit.apis;
 
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import java.nio.ByteBuffer;
+
 /**
+ * Converts an image input into a String output.
  * Created by Tyler on 4/12/2017.
  */
 
-public class ImageProcessor {
+public class ImageProcessor extends AppCompatActivity {
 
-    byte[] input;
+    public static final int NV16 = 16, NV21 = 17, YV12 = 842094169;
+    private final Frame.Builder frameBuilder = new Frame.Builder();
+    private final String NOTEXT = "No text detected";
 
-    String convertInputToText(){
-        return input.toString();
+    /**
+     * Gets text from an image's ByteBuffer, width, height, and format
+     * @param data ByteBuffer data from image
+     * @param width Image width (pixels?)
+     * @param height Image height (pixels?)
+     * @param format Byte format. Use the values present in ImageProcessor.
+     */
+    public String convertDataToText(ByteBuffer data, int width, int height, int format) {
+        frameBuilder.setImageData(data, width, height, format);
+        return convert();
     }
 
-    void saveResult(){
-
+    /**
+     * Gets text from a Bitmap object
+     * @param bitmap Bitmap object of image
+     */
+    public String convertBitmapToText(Bitmap bitmap){
+        frameBuilder.setBitmap(bitmap);
+        return convert();
     }
+
+    private String convert(){
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        Frame f = frameBuilder.build();
+        SparseArray<TextBlock> items = textRecognizer.detect(f);
+        String result = "";
+        for (int i = 0; i < items.size(); ++i){
+            TextBlock item = items.get(i);
+            result = result.concat(item.getValue());
+        }
+        if (result.isEmpty()) result = NOTEXT;
+        textRecognizer.release();
+        return result;
+    }
+
 }
